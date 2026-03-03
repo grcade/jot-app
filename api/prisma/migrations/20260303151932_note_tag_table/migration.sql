3aee29860ed224095b2afb197503f6522f3b47c5
gcrade <gauravkm678@gmail.com>
2026-03-03 21:00:21 +0530

    schema changes

---
+++ api/prisma/migrations/20260303151932_note_tag_table/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `TaskTag` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "TaskTag" DROP CONSTRAINT "TaskTag_tagId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TaskTag" DROP CONSTRAINT "TaskTag_taskId_fkey";
+
+-- AlterTable
+ALTER TABLE "Task" ALTER COLUMN "priority" DROP NOT NULL,
+ALTER COLUMN "deadline" DROP NOT NULL;
+
+-- DropTable
+DROP TABLE "TaskTag";
+
+-- CreateTable
+CREATE TABLE "NoteTag" (
+    "noteId" TEXT NOT NULL,
+    "tagId" TEXT NOT NULL,
+
+    CONSTRAINT "NoteTag_pkey" PRIMARY KEY ("noteId","tagId")
+);
+
+-- AddForeignKey
+ALTER TABLE "NoteTag" ADD CONSTRAINT "NoteTag_noteId_fkey" FOREIGN KEY ("noteId") REFERENCES "Note"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NoteTag" ADD CONSTRAINT "NoteTag_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
